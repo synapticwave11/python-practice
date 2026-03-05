f2ce6cf02e2418942cc5146d34a8940bf2eaf611
Alia <singhalia2008@gmail.com>
2026-03-05 21:23:51 +0530

    Add files via upload

---
+++ Arithmetic_operations.py
@@ -0,0 +1,15 @@
+print("small operation-1")
+print("Basic Arithmetic")
+
+# print addtion , subtraction , multiplication and division of two created variables.
+a=11
+b=2
+print("Addition:", (a+b))
+print("Subraction:",(a-b))
+print("Multiplication:",(a*b))
+print("Division:",(a/b))
+
+#Squares and cubes
+
+n=4
+print("Square")
